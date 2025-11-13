2caba3694c133891b9acd3fc97aff2a34709114c
cxh <2604856589@qq.com>
2025-11-13 09:39:57 +0800

    feat

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -9,18 +9,11 @@
     </p>
   </div>
 
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank">create-vue</a>, the official Vue + Vite starter
-  </p>
-  <p>
-    Learn more about IDE Support for Vue in the
-    <a href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support" target="_blank">Vue Docs Scaling up Guide</a>.
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <Button type="primary">Primary Button</Button>
 </template>
 
 <script setup lang="ts">
+  import { Button } from 'ant-design-vue';
   import { ref } from 'vue';
 
   defineProps<{ msg: string }>();
